f3ebb24b508bff53c6f639711393fab6a459558d
Aravinda Rathnamalala <aravinda_r@epic.lk>
2019-02-25 22:31:37 +0530

    Initial commit

---
+++ app/src/main/java/com/example/aravinda_r/reactnativetest/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.aravinda_r.reactnativetest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
